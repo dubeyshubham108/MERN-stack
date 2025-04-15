553497f8b7c50010b5dd12e460904bbf4f8725de
dubeyshubham108 <dubeyshubham154@gmail.com>
2025-04-15 21:45:20 +0530

    Nested checkboxes

---
+++ Machine Coding Problems/Nested Checkbox/src/App.jsx
@@ -0,0 +1,52 @@
+import './App.css'
+
+const checkboxesData = [
+  {
+    id: 1,
+    name: "Fruits",
+    children: [
+      {
+        id: 2,
+        name: "Citrus",
+        children: [
+          {
+            id: 3,
+            name: "Orange",
+          },
+          {
+            id: 4,
+            name: "Lemon",
+          },
+        ],
+      },
+      {
+        id: 5,
+        name: "Berries",
+        children: [
+          {
+            id: 6,
+            name: "Strawberry",
+          },
+          {
+            id: 7,
+            name: "Blueberry",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const Checkboxes = ( data ) => {
+  return;
+}
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
